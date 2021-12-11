9597880ff29e59a990eb4173d14ac78fafeff33f
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-12-11 13:36:16 +0530

    Update forcesub.py

--- helper_funcs/forcesub.py
+++ helper_funcs/forcesub.py
@@ -21,11 +21,11 @@ async def ForceSub(bot: Client, cmd: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            text="**Please Join My Updates Channel to use me!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url="https://t.me/TeleRoidGroup")
+                        InlineKeyboardButton("Join Updates Channel", url="https://t.me/pyrogrammers")
                     ]
                 ]
             ),
